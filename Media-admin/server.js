98d59df1d2336922ff2e70e24ebe1c45fe31e382
YoinkyDonkey <baduyyy.aquatique@gmail.com>
2026-02-13 20:38:44 -0500

    Change server listen address to 0.0.0.0

--- Media-admin/server.js
+++ Media-admin/server.js
@@ -86,4 +86,7 @@ app.delete('/delete/:filename', (req, res) => {
 });
 
 // ------------------ Start Server ------------------
-app.listen(PORT, () => console.log(`Server running on http://localhost:${PORT}`));
+// ------------------ Start Server ------------------
+app.listen(PORT, "0.0.0.0", () => {
+  console.log(`Server running on http://0.0.0.0:${PORT}`);
+});
